a3771a132646253ae7177820dcfbf6a722bf7877
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2024-11-07 21:44:14 -0300

    Created Menu and Conta classes

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,9 +1,17 @@
 package conta;
 
+import conta.model.Conta;
+
 public class Menu {
 
 	public static void main(String[] args) {
 
+		Conta conta1 = new Conta("Nome do Titular", 123, 456, 02, 500.50f);
+		Conta conta2 = new Conta("Nome do Cliente", 0123, 0456, 01, 800.70f);
+
+		conta1.Imprimir();
+		conta2.Imprimir();	
+		
 	}
 
 }
